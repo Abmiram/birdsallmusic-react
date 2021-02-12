c512000cdaff2e232692c6f192a2baabeae73fbc
Ross Birdsall <ross.birdsall9@gmail.com>
2021-02-12 14:29:06 -0500

    Basic Header component

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button } from 'reactstrap';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isNavOpen: false
+        }
+
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <header>
+                    <Jumbotron fluid>
+                        <div className="container">
+                            <div className="row text-md-left text-center">
+                                <div className="col col-md-4 light-background">
+                                    <h1>Birdsall</h1>
+                                    <h2>Music Studios</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </Jumbotron>
+                </header>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Header;
