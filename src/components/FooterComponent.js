835a38d4fc95df123b4f20a8e2dc8fc25c21ad31
Ross Birdsall <ross.birdsall9@gmail.com>
2021-02-13 13:20:52 -0500

    Footer links

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
     constructor(props) {
@@ -12,23 +13,22 @@ class Footer extends Component {
                     <div className="row align-items-center">
                         <div className="col-2">
                             <ul className="list-unstyled">
-                                <li>Home</li>
-                                <li>About</li>
-                                <li>Music</li>
-                                <li>Students</li>
-                                <li>Contact</li>
+                                <li><Link to="/home">Home</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                                <li><Link to="/music">Music</Link></li>
+                                <li><Link to="/students">Students</Link></li>
+                                <li><Link to="/contact">Contact</Link></li>
                             </ul>
                         </div>
                         <div className="col-10 text-right">
-                            <a role="button" className="btn btn-link" href="tel:+7194477237">
-                                <p><i className="fa fa-phone"></i>
-                            1-719-447-7237</p>
+                            <a href="tel:+7194477237">
+                                <i className="fa fa-phone" /> 1-719-447-7237
                             </a><br />
-                            <a role="button" className="btn btn-link" href="mailto:ross.birdsall9@gmail.com">
-                                <p><i className="fa fa-envelope-o"></i> ross.birdsall9@gmail.com</p>
+                            <a href="mailto:ross.birdsall9@gmail.com">
+                                <i className="fa fa-envelope-o" /> ross.birdsall9@gmail.com
                             </a><br />
-                            <a role="button" className="btn btn-link" href="https://www.linkedin.com/in/ross-birdsall-85950311b/">
-                                <p>Website by Ross Birdsall</p>
+                            <a href="https://www.linkedin.com/in/ross-birdsall-85950311b/">
+                                Website by Ross Birdsall
                             </a>
                         </div>
                     </div>
